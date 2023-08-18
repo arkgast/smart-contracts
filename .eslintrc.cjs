2fb79254de65772fd5da29d2551daaa90680ff60
Arnold Gandarillas Castillo <arkgast@gmail.com>
2023-08-18 13:12:04 -0400

    chore: fix eslint & remove front boilerplate

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,4 @@
 module.exports = {
-  root: true,
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   ignorePatterns: ["artifacts", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
