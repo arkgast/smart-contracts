2596c76264b8d56f46a2b679f9b8a616a51dceb1
Arnold Gandarillas Castillo <arkgast@gmail.com>
2023-08-21 11:57:53 -0400

    feat: update deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,22 +1,18 @@
-import { ethers } from "hardhat";
+import { ethers, run } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const contractName = "SimpleStorage";
+  const simpleStorageFactory = await ethers.getContractFactory(contractName);
+  const simpleStorage = await simpleStorageFactory.deploy();
+  await simpleStorage.waitForDeployment();
 
-  const lockedAmount = ethers.parseEther("0.001");
+  const contractAddress = await simpleStorage.getAddress();
+  console.log("SimpleStorage deployed to:", contractAddress);
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
+  run("export-abi", {
+    contractName: contractName,
+    contractAddress: contractAddress,
   });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
